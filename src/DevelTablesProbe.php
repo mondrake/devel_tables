f518c8136467729e790b42c41b0ebbdc61f58fde
mondrake <mondrake@mondrake.org>
2016-03-03 08:36:41 +0100

    Start implementing Doctrine DBAL

--- src/DevelTablesProbe.php
+++ src/DevelTablesProbe.php
@@ -9,7 +9,8 @@ namespace Drupal\devel_tables;
 
 use Drupal\Core\Cache\Cache;
 use Drupal\Core\Database\Database;
-use Drupal\devel_tables\drupalTableObj;
+use Doctrine\DBAL\Configuration;
+use Doctrine\DBAL\DriverManager;
 
 /**
  * @todo
@@ -71,30 +72,76 @@ class DevelTablesProbe {
       }
     }
 
-    // get all tables in the $connection database
-    $dto = new drupalTableObj($connection);
-    $db_tables = $dto->fetchAllTables();
+    // Get all tables in the $connection database.
+    $config = new Configuration();
+    $options = Database::getConnection()->getConnectionOptions();
+    $connectionParams = [
+      'dbname' => $options['database'],
+      'user' => $options['username'],
+      'password' => $options['password'],
+      'host' => $options['host'],
+      'driver' => 'pdo_mysql',
+    ];
+    $conn = DriverManager::getConnection($connectionParams, $config);
+    $dbal_tables = $conn->getSchemaManager()->listTables();
 
+    // @todo generalise for other db
+    $recs = $conn->query("show table status")->fetchAll();
+    $db_tables_extra = [];
+    foreach ($recs as $rec) {
+      $db_tables_extra[$rec['Name']] = $rec;
+    }
     $table_list = [];
 
+    foreach ($dbal_tables as $dbal_table) {
+      $table_name = $dbal_table->getName();
+        if (strpos($table_name, $table_prefix) === 0) {
+          $base_table_name = substr($table_name, strlen($table_prefix), strlen($table_name) - strlen($table_prefix));
+          $table_list[$table_name] = array(
+            'isDrupal' => true,
+            'full_name' => $table_name,
+            'prefix' => $table_prefix,
+            'name' => $base_table_name,
+            'module' => empty($schema_tables[$table_name]['module']) ? '???' : $schema_tables[$table_name]['module'],
+            'description' => empty($db_tables_extra[$table_name]['Comment']) ? t('*** No description available ***'): $db_tables_extra[$table_name]['Comment'],
+            'rowsCount' => $db_tables_extra[$table_name]['Rows'],
+            'collation' => $db_tables_extra[$table_name]['Collation'],
+            'storageMethod' => $db_tables_extra[$table_name]['Engine'],
+          );
+        }
+        else {
+          $table_list[$table_name] = array(
+            'isDrupal' => true,
+            'full_name' => $table_name,
+            'prefix' => NULL,
+            'name' => $table_name,
+            'module' => empty($schema_tables[$table_name]['module']) ? '???' : $schema_tables[$table_name]['module'],
+            'description' => empty($db_tables_extra[$table_name]['Comment']) ? t('*** No description available ***'): $db_tables_extra[$table_name]['Comment'],
+            'rowsCount' => $db_tables_extra[$table_name]['Rows'],
+            'collation' => $db_tables_extra[$table_name]['Collation'],
+            'storageMethod' => $db_tables_extra[$table_name]['Engine'],
+          );
+        }
+    }
+
     // Firstly process schema supported tables.
-    foreach ($db_tables as $table_name => $table_db_details) {
+ /*   foreach ($db_tables as $table_name => $table_db_details) {
       foreach ($prefixes as $prefix => $d) {
-        $tablePrefix = ($prefix == '!null!') ? null : $prefix;
-        if (!$tablePrefix or strpos($table_name, $tablePrefix) === 0) {
-          if ($tablePrefix) {
-            $unPrefixedTN = substr($table_name, strlen($tablePrefix), strlen($table_name) - strlen($tablePrefix));
+        $table_prefix = ($prefix == '!null!') ? null : $prefix;
+        if (!$table_prefix or strpos($table_name, $table_prefix) === 0) {
+          if ($table_prefix) {
+            $base_table_name = substr($table_name, strlen($table_prefix), strlen($table_name) - strlen($table_prefix));
           } else {
-            $unPrefixedTN = $table_name;
+            $base_table_name = $table_name;
           }
-          if (isset($schema_tables[$unPrefixedTN])) {
+          if (isset($schema_tables[$base_table_name])) {
             $row = array(
               'isDrupal' => true,
               'full_name' => $table_name,
-              'prefix' => $schema_tables[$unPrefixedTN]['prefix'],
-              'name' => $unPrefixedTN,
-              'module' => isset($schema_tables[$unPrefixedTN]['module']) ? $schema_tables[$unPrefixedTN]['module'] : t('Drupal table ?'),
-              'description' => isset($schema_tables[$unPrefixedTN]['description']) ? $schema_tables[$unPrefixedTN]['description'] : NULL,
+              'prefix' => $schema_tables[$base_table_name]['prefix'],
+              'name' => $base_table_name,
+              'module' => isset($schema_tables[$base_table_name]['module']) ? $schema_tables[$base_table_name]['module'] : t('Drupal table ?'),
+              'description' => isset($schema_tables[$base_table_name]['description']) ? $schema_tables[$base_table_name]['description'] : NULL,
               'rowsCount' => $table_db_details['rows'],
               'collation' => $table_db_details['collation'],
               'storageMethod' => $table_db_details['storageMethod'],
@@ -106,24 +153,22 @@ class DevelTablesProbe {
       }
     }
 
-    // @todo find it in config
-    $prefix = 'lab05_';
-
     // Secondly process the rest db tables.
-    foreach ($db_tables as $table_name => $table_db_details) {
+    foreach ($db_tables as $x => $table_db_details) {
+      $table_name = $table_db_details->getName();
       if (!isset($table_list[$table_name])) {
-        if (strpos($table_name, $tablePrefix) === 0) {
-          $unPrefixedTN = substr($table_name, strlen($prefix), strlen($table_name) - strlen($prefix));
+        if (strpos($table_name, $table_prefix) === 0) {
+          $base_table_name = substr($table_name, strlen($table_prefix), strlen($table_name) - strlen($table_prefix));
           $row = array(
             'isDrupal' => true,
             'full_name' => $table_name,
-            'prefix' => $prefix,
-            'name' => $unPrefixedTN,
+            'prefix' => $table_prefix,
+            'name' => $base_table_name,
             'module' => '???',
-            'description' => $table_db_details['description'] ?: t('*** No description available ***'),
-            'rowsCount' => $table_db_details['rows'],
-            'collation' => $table_db_details['collation'],
-            'storageMethod' => $table_db_details['storageMethod'],
+            'description' => '',//$table_db_details['description'] ?: t('*** No description available ***'),
+            'rowsCount' => '',//$table_db_details['rows'],
+            'collation' => '',//$table_db_details['collation'],
+            'storageMethod' => '',//$table_db_details['storageMethod'],
           );
         }
         else {
@@ -133,10 +178,10 @@ class DevelTablesProbe {
             'prefix' => null,
             'name' => $table_name,
             'module' => NULL,
-            'description' => $table_db_details['description'] ?: t('*** No description available ***'),
-            'rowsCount' => $table_db_details['rows'],
-            'collation' => $table_db_details['collation'],
-            'storageMethod' => $table_db_details['storageMethod'],
+            'description' => '',//$table_db_details['description'] ?: t('*** No description available ***'),
+            'rowsCount' => '',//$table_db_details['rows'],
+            'collation' => '',//$table_db_details['collation'],
+            'storageMethod' => '',//$table_db_details['storageMethod'],
           );
         }
         $table_list[$table_name] = $row;
@@ -147,7 +192,7 @@ class DevelTablesProbe {
         }
       }
     }
-
+*/
     ksort($table_list);
     return $table_list;
   }
